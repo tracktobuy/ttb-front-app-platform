73b713fc8f2eb21a368451bf07283342150f84f4
George Nunes <georgebnunes@gmail.com>
2026-03-24 08:46:37 -0300

    build: define output folder

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  output: "export",
+  distDir: 'dist',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
