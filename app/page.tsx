dc3cfc424af358d8c6194a86e121cc1ab8c14ec8
George Nunes <georgebnunes@gmail.com>
2026-07-22 14:23:17 -0300

    feat: redirect login

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 import Image from "next/image"
 import Link from "next/link"
+import { createClient } from "@/utils/supabase/server"
+import { redirect } from "next/navigation"
+import { User, Sparkles, ShieldCheck } from "lucide-react"
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
 
   return (
 
